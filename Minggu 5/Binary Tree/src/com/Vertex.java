abf32657d13d2f090d88a4d42bfd0b14a0655732
unknown <muhamadkbar47@gmail.com>
2021-04-10 22:59:01 +0700

    TugasMinggu5

---
+++ Minggu 5/Binary Tree/src/com/Vertex.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Vertex {
+    public char label;
+    public boolean wasVisited;
+    
+    public Vertex(char label) {
+        this.label = label;
+        wasVisited = false;
+    }
+}
